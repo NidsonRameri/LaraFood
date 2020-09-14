169be69b5d5b067c33fa0837ef9a350411500e0a
nidson <nidson1@hotmail.com>
2020-09-14 20:22:17 -0300

    Generate QrCode Table

--- resources/views/admin/pages/tables/index.blade.php
+++ resources/views/admin/pages/tables/index.blade.php
@@ -39,6 +39,7 @@
                             <td style='width=10px;'>
                                 <a href="{{route('tables.show', $table->id)}}" class="btn btn-info"><i class="fas fa-angle-double-right"></i> Ver</a>
                                 <a href="{{route('tables.edit', $table->id)}}" class="btn btn-warning"><i class="far fa-edit"></i> Editar</a>
+                                <a href="{{route('tables.qrcode', $table->identify)}}" class="btn btn-default" target="_blank"><i class="fas fa-qrcode"></i></a>
                             </td>
                         </tr>
                     @endforeach 
